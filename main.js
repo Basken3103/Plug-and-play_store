82e89f252a2046b7a4090048777fa5c7c51d0c08
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-05-31 13:47:04 +0200

    Jeg er næsten færdig med min how to play side. Da der er nogle ting jeg skal have hjælp til, går jeg i nu i gang med kontaktsiden.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const modal = document.getElementById("videoModal");
+const iframe = document.getElementById("videoFrame");
+const closeBtn = document.querySelector(".close");
+
+function openVideo(url) {
+    iframe.src = url;
+    modal.style.display = "flex";
+}
+
+closeBtn.onclick = function () {
+    modal.style.display = "none";
+    iframe.src = ""; // Stop videoafspilning
+}
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+        iframe.src = "";
+    }
+}
 
